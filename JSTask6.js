7a407a1b6fde802c6cee8c193de564f6d4bcee1d
Lakshya Goel <lgoel340@gmail.com>
2022-11-13 00:30:44 +0530

    JS Task 6

--- JSTask6.js
+++ JSTask6.js
@@ -1,4 +1,5 @@
 // Write a javascript function for the Fibonacci generator with the next() method in Generators and print up to 5 values.
+
 function *fibonacci(n, current = 0, next = 1)
 {
     if (n === 0)
